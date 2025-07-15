db7118fd5a56216c386fe98bbfd777fb8864bd5a
Eliton Peroni <eliton.peroni@kognit.com.br>
2025-07-15 00:12:43 -0300

    adjustment response

--- src/components/data-table.tsx
+++ src/components/data-table.tsx
@@ -104,12 +104,14 @@ import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 
 export const schema = z.object({
   id: z.number(),
-  code: z.string(),
-  header: z.string(),
-  type: z.string(),
-  status: z.string(),
-  limit: z.string(),
-  reviewer: z.string(),
+  stock: z.string(),
+  name: z.string(),
+  logo: z.string(),
+  close: z.number(),
+  change: z.number(),
+  volume: z.number(),
+  market_cap: z.number(),
+  sector: z.string(),
 });
 
 // Create a separate component for the drag handle
@@ -170,10 +172,7 @@ const columns: ColumnDef<z.infer<typeof schema>>[] = [
     cell: ({ row }) => {
       return (
         <Avatar className="w-14 h-14">
-          <AvatarImage
-            src="https://icons.brapi.dev/icons/ITSA4.svg"
-            className="w-14 h-14"
-          />
+          <AvatarImage src={row.original.logo} className="w-14 h-14" />
         </Avatar>
       );
     },
@@ -183,30 +182,86 @@ const columns: ColumnDef<z.infer<typeof schema>>[] = [
     accessorKey: "stock",
     header: "Código",
     cell: ({ row }) => {
-       return <div >
-        <div className="text-sm font-medium">
-          {row.original.code}
+      return (
+        <div>
+          <div className="text-sm font-medium">{row.original.stock}</div>
         </div>
-      </div>
+      );
     },
     enableHiding: false,
   },
   {
     accessorKey: "name",
     header: "Nome",
-    cell: ({ row }) => {
-      return <TableCellViewer item={row.original} />;
+      cell: ({ row }) => {
+      return (
+        <div>
+          <div className="text-sm font-medium">{row.original.name}</div>
+        </div>
+      );
     },
     enableHiding: false,
   },
-  // {
-  //   accessorKey: "close",
-  //   header: "Preço fechamento R$",
-  //   cell: ({ row }) => {
-  //     return <TableCellViewer item={row.original.code} />;
-  //   },
-  //   enableHiding: false,
-  // },
+  {
+    accessorKey: "close",
+    header: "Preço fechamento R$",
+    cell: ({ row }) => {
+      return (
+        <div>
+          <div className="text-sm font-medium">{row.original.close}</div>
+        </div>
+      );
+    },
+    enableHiding: false,
+  },
+  {
+    accessorKey: "change",
+    header: "Variação R$",
+    cell: ({ row }) => {
+      return (
+        <div>
+          <div className="text-sm font-medium">{row.original.change.toFixed(2)}</div>
+        </div>
+      );
+    },
+    enableHiding: false,
+  },
+  {
+    accessorKey: "id",
+    header: "Volume R$",
+    cell: ({ row }) => {
+      return (
+        <div>
+          <div className="text-sm font-medium">{row.original.volume.toFixed(2)}</div>
+        </div>
+      );
+    },
+    enableHiding: false,
+  },
+  {
+    accessorKey: "volume",
+    header: "Capital R$",
+    cell: ({ row }) => {
+      return (
+        <div>
+          <div className="text-sm font-medium">{row.original.market_cap.toFixed(2)}</div>
+        </div>
+      );
+    },
+    enableHiding: false,
+  },
+  {
+    accessorKey: "sector",
+    header: "Setor R$",
+    cell: ({ row }) => {
+      return (
+        <div>
+          <div className="text-sm font-medium">{row.original.sector}</div>
+        </div>
+      );
+    },
+    enableHiding: false,
+  },
   // {
   //   accessorKey: "type",
   //   header: "Section Type",
@@ -218,104 +273,104 @@ const columns: ColumnDef<z.infer<typeof schema>>[] = [
   //     </div>
   //   ),
   // },
-  {
-    accessorKey: "status",
-    header: "Status",
-    cell: ({ row }) => (
-      <Badge variant="outline" className="text-muted-foreground px-1.5">
-        {row.original.status === "Done" ? (
-          <IconCircleCheckFilled className="fill-green-500 dark:fill-green-400" />
-        ) : (
-          <IconLoader />
-        )}
-        {row.original.status}
-      </Badge>
-    ),
-  },
-  {
-    accessorKey: "target",
-    header: () => <div className="w-full text-right">Target</div>,
-    cell: ({ row }) => (
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          toast.promise(new Promise((resolve) => setTimeout(resolve, 1000)), {
-            loading: `Saving ${row.original.header}`,
-            success: "Done",
-            error: "Error",
-          });
-        }}
-      >
-        <Label htmlFor={`${row.original.id}-target`} className="sr-only">
-          Target
-        </Label>
-        <Input
-          className="hover:bg-input/30 focus-visible:bg-background dark:hover:bg-input/30 dark:focus-visible:bg-input/30 h-8 w-16 border-transparent bg-transparent text-right shadow-none focus-visible:border dark:bg-transparent"
-          // defaultValue={row.original.target}
-          id={`${row.original.id}-target`}
-        />
-      </form>
-    ),
-  },
-  {
-    accessorKey: "limit",
-    header: () => <div className="w-full text-right">Limit</div>,
-    cell: ({ row }) => (
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          toast.promise(new Promise((resolve) => setTimeout(resolve, 1000)), {
-            loading: `Saving ${row.original.header}`,
-            success: "Done",
-            error: "Error",
-          });
-        }}
-      >
-        <Label htmlFor={`${row.original.id}-limit`} className="sr-only">
-          Limit
-        </Label>
-        <Input
-          className="hover:bg-input/30 focus-visible:bg-background dark:hover:bg-input/30 dark:focus-visible:bg-input/30 h-8 w-16 border-transparent bg-transparent text-right shadow-none focus-visible:border dark:bg-transparent"
-          defaultValue={row.original.limit}
-          id={`${row.original.id}-limit`}
-        />
-      </form>
-    ),
-  },
-  {
-    accessorKey: "reviewer",
-    header: "Reviewer",
-    cell: ({ row }) => {
-      const isAssigned = row.original.reviewer !== "Assign reviewer";
+  // {
+  //   accessorKey: "status",
+  //   header: "Status",
+  //   cell: ({ row }) => (
+  //     <Badge variant="outline" className="text-muted-foreground px-1.5">
+  //       {row.original.status === "Done" ? (
+  //         <IconCircleCheckFilled className="fill-green-500 dark:fill-green-400" />
+  //       ) : (
+  //         <IconLoader />
+  //       )}
+  //       {row.original.status}
+  //     </Badge>
+  //   ),
+  // },
+  // {
+  //   accessorKey: "target",
+  //   header: () => <div className="w-full texts-right">Target</div>,
+  //   cell: ({ row }) => (
+  //     <form
+  //       onSubmit={(e) => {
+  //         e.preventDefault();
+  //         toast.promise(new Promise((resolve) => setTimeout(resolve, 1000)), {
+  //           loading: `Saving ${row.original.header}`,
+  //           success: "Done",
+  //           error: "Error",
+  //         });
+  //       }}
+  //     >
+  //       <Label htmlFor={`${row.original.id}-target`} className="sr-only">
+  //         Target
+  //       </Label>
+  //       <Input
+  //         className="hover:bg-input/30 focus-visible:bg-background dark:hover:bg-input/30 dark:focus-visible:bg-input/30 h-8 w-16 border-transparent bg-transparent text-right shadow-none focus-visible:border dark:bg-transparent"
+  //         // defaultValue={row.original.target}
+  //         id={`${row.original.id}-target`}
+  //       />
+  //     </form>
+  //   ),
+  // },
+  // {
+  //   accessorKey: "limit",
+  //   header: () => <div className="w-full text-right">Limit</div>,
+  //   cell: ({ row }) => (
+  //     <form
+  //       onSubmit={(e) => {
+  //         e.preventDefault();
+  //         toast.promise(new Promise((resolve) => setTimeout(resolve, 1000)), {
+  //           loading: `Saving ${row.original.header}`,
+  //           success: "Done",
+  //           error: "Error",
+  //         });
+  //       }}
+  //     >
+  //       <Label htmlFor={`${row.original.id}-limit`} className="sr-only">
+  //         Limit
+  //       </Label>
+  //       <Input
+  //         className="hover:bg-input/30 focus-visible:bg-background dark:hover:bg-input/30 dark:focus-visible:bg-input/30 h-8 w-16 border-transparent bg-transparent text-right shadow-none focus-visible:border dark:bg-transparent"
+  //         defaultValue={row.original.limit}
+  //         id={`${row.original.id}-limit`}
+  //       />
+  //     </form>
+  //   ),
+  // },
+  // {
+  //   accessorKey: "reviewer",
+  //   header: "Reviewer",
+  //   cell: ({ row }) => {
+  //     const isAssigned = row.original.reviewer !== "Assign reviewer";
 
-      if (isAssigned) {
-        return row.original.reviewer;
-      }
+  //     if (isAssigned) {
+  //       return row.original.reviewer;
+  //     }
 
-      return (
-        <>
-          <Label htmlFor={`${row.original.id}-reviewer`} className="sr-only">
-            Reviewer
-          </Label>
-          <Select>
-            <SelectTrigger
-              className="w-38 **:data-[slot=select-value]:block **:data-[slot=select-value]:truncate"
-              size="sm"
-              id={`${row.original.id}-reviewer`}
-            >
-              <SelectValue placeholder="Assign reviewer" />
-            </SelectTrigger>
-            <SelectContent align="end">
-              <SelectItem value="Eddie Lake">Eddie Lake</SelectItem>
-              <SelectItem value="Jamik Tashpulatov">
-                Jamik Tashpulatov
-              </SelectItem>
-            </SelectContent>
-          </Select>
-        </>
-      );
-    },
-  },
+  //     return (
+  //       <>
+  //         <Label htmlFor={`${row.original.id}-reviewer`} className="sr-only">
+  //           Reviewer
+  //         </Label>
+  //         <Select>
+  //           <SelectTrigger
+  //             className="w-38 **:data-[slot=select-value]:block **:data-[slot=select-value]:truncate"
+  //             size="sm"
+  //             id={`${row.original.id}-reviewer`}
+  //           >
+  //             <SelectValue placeholder="Assign reviewer" />
+  //           </SelectTrigger>
+  //           <SelectContent align="end">
+  //             <SelectItem value="Eddie Lake">Eddie Lake</SelectItem>
+  //             <SelectItem value="Jamik Tashpulatov">
+  //               Jamik Tashpulatov
+  //             </SelectItem>
+  //           </SelectContent>
+  //         </Select>
+  //       </>
+  //     );
+  //   },
+  // },
   {
     id: "actions",
     cell: () => (
@@ -344,7 +399,7 @@ const columns: ColumnDef<z.infer<typeof schema>>[] = [
 
 function DraggableRow({ row }: { row: Row<z.infer<typeof schema>> }) {
   const { transform, transition, setNodeRef, isDragging } = useSortable({
-    id: row.original.id,
+    id: row.original.stock,
   });
 
   return (
@@ -685,12 +740,12 @@ function TableCellViewer({ item }: { item: z.infer<typeof schema> }) {
     <Drawer direction={isMobile ? "bottom" : "right"}>
       <DrawerTrigger asChild>
         <Button variant="link" className="text-foreground w-fit px-0 text-left">
-          {item.header}
+          {/* {item.header} */}
         </Button>
       </DrawerTrigger>
       <DrawerContent>
         <DrawerHeader className="gap-1">
-          <DrawerTitle>{item.header}</DrawerTitle>
+          {/* <DrawerTitle>{item.header}</DrawerTitle> */}
           <DrawerDescription>
             Showing total visitors for the last 6 months
           </DrawerDescription>
@@ -756,12 +811,12 @@ function TableCellViewer({ item }: { item: z.infer<typeof schema> }) {
           <form className="flex flex-col gap-4">
             <div className="flex flex-col gap-3">
               <Label htmlFor="header">Header</Label>
-              <Input id="header" defaultValue={item.header} />
+              {/* <Input id="header" defaultValue={item.header} /> */}
             </div>
             <div className="grid grid-cols-2 gap-4">
               <div className="flex flex-col gap-3">
                 <Label htmlFor="type">Type</Label>
-                <Select defaultValue={item.type}>
+                <Select defaultValue={item.logo}>
                   <SelectTrigger id="type" className="w-full">
                     <SelectValue placeholder="Select a type" />
                   </SelectTrigger>
@@ -787,7 +842,7 @@ function TableCellViewer({ item }: { item: z.infer<typeof schema> }) {
               </div>
               <div className="flex flex-col gap-3">
                 <Label htmlFor="status">Status</Label>
-                <Select defaultValue={item.status}>
+                <Select defaultValue={item.code}>
                   <SelectTrigger id="status" className="w-full">
                     <SelectValue placeholder="Select a status" />
                   </SelectTrigger>
@@ -806,12 +861,12 @@ function TableCellViewer({ item }: { item: z.infer<typeof schema> }) {
               </div>
               <div className="flex flex-col gap-3">
                 <Label htmlFor="limit">Limit</Label>
-                <Input id="limit" defaultValue={item.limit} />
+                <Input id="limit" defaultValue={item.code} />
               </div>
             </div>
             <div className="flex flex-col gap-3">
               <Label htmlFor="reviewer">Reviewer</Label>
-              <Select defaultValue={item.reviewer}>
+              <Select defaultValue={item.code}>
                 <SelectTrigger id="reviewer" className="w-full">
                   <SelectValue placeholder="Select a reviewer" />
                 </SelectTrigger>
