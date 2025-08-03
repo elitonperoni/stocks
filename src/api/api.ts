a364e64b67003f1d601625b601c0c89dd34b91a7
Eliton Peroni <eliton.peroni@kognit.com>
2025-08-03 11:18:32 -0300

    deploy

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://localhost:5001/',
+  baseURL: 'http://web-api/8080/', //'https://localhost:5001/',
   timeout: 20000,
   headers: {
     'Content-Type': 'application/json',
